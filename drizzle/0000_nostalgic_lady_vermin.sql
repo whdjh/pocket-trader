b89735e1b9c068a8b9b87a24c02738f6f12bf786
whdjh <wngns9807@gmail.com>
2026-01-03 21:44:58 +0900

    chore: 전체 초기화(AI 자동화로 변경)

--- drizzle/0000_nostalgic_lady_vermin.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "users" (
-	"pk" serial PRIMARY KEY NOT NULL,
-	"id" varchar(255) NOT NULL,
-	"password" varchar(255) NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "users_id_unique" UNIQUE("id")
-);
